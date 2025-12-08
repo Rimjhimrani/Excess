c5fee7bf2b3bce0f7a2ae05f14a17ee1c6ebe6c9
Rimjhimrani <144531476+Rimjhimrani@users.noreply.github.com>
2025-12-08 10:45:29 +0530

    Update Inventory.py

--- Inventory.py
+++ Inventory.py
@@ -283,7 +283,7 @@ class InventoryAnalyzer:
         
         part_nos = [x['PART NO'] for x in sorted_parts]
         descriptions = [x['PART DESCRIPTION'] for x in sorted_parts]
-        vendor name = [x['Vendor Name'] for x in sorted_parts]
+        vendor_name = [x['Vendor Name'] for x in sorted_parts]
         values = [x['Stock Deviation Value'] for x in sorted_parts]
         
         if value_format == 'lakhs':
